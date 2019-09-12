7ce57fe242f9e0cdc85bb095e60127a400261fb4
Jon Holmes <holmjona@isu.edu>
2019-09-12 12:15:38 -0600

    Stuff about JavaScript.

---
+++ scripts/learning.js
@@ -0,0 +1,49 @@
+﻿//alert("From file");
+
+var a = 5;
+var b = 7;
+var c;
+
+var d = a + b;
+var e = (a + b);
+var f = (a) + (b);
+var g = (((a)) + ((b)));
+
+//if (typeof c === "undefined") {
+//    alert("Oops c needs a value");
+//}
+c = " apples ";
+//alert("a=" + a + "b=" + b + "c=" + c);
+//alert(a + b + c + (a + b) + c);
+//alert(c + " of" + (b + a));
+
+//alert(typeof a);
+//a = "happy";
+//alert(typeof a);
+
+//var h = 5
+//var i = 8
+//alert(h + i); // works but don't ;
+
+//sayIt();
+//function sayIt() {
+//    alert("it");
+//}
+
+//function addStuff(x, y) {
+//    return x + y;
+//}
+
+////alert(addStuff(a, addStuff(b,c,"And more stuff")));
+//alert(addStuff(8));
+
+var arr = [1,2,3];
+var arr2 = new Array(1,2,3);
+
+//alert(arr);
+//alert(arr2);
+var arr3 = [];
+arr3[0] = 4;
+arr3[3] = 56;
+arr3[arr3[3]] = 7987;
+alert(arr3);
---
+++ scripts/learning_last.js
@@ -0,0 +1,1 @@
+﻿//alert("I should happen last");
