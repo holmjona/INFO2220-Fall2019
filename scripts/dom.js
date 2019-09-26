1151c45401044109bd09945d6e4a5171dd48b76f
Jon Holmes <holmjona@isu.edu>
2019-09-26 12:21:29 -0600

    Added discussion about DOM.

---
+++ scripts/dom.js
@@ -0,0 +1,27 @@
+﻿var pNew = document.getElementById("pOne");
+var pPar = pNew.parentNode;
+var pPNext = pNew.parentNode.nextSibling;
+var pPPrev = pNew.parentNode.previousSibling;
+var pNext = pNew.nextSibling;
+var pPrev = pNew.previousSibling;
+
+var pPrevElem = getNext(pNew);
+
+var newUL = document.createElement("ul");
+for (var i = 0; i < 6; i++) {
+    var li1 = document.createElement("li");
+    li1.textContent = "Boo "+ i;
+    newUL.appendChild(li1);
+}
+
+
+pPrevElem.appendChild(newUL);
+var pmagic = "i don't know";
+
+function getNext(ele) {
+    var next = ele.nextSibling;
+    while (next.nodeName === "#text") {
+        next = next.nextSibling;
+    }
+    return next;
+}
