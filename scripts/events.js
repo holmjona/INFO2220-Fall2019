c0e5e562a1aa098e5ac7d615c5c774dc603788dc
Jon Holmes <holmjona@isu.edu>
2019-10-03 12:19:02 -0600

    Added more event stuff with bubbling and delegation.

---
+++ scripts/events.js
@@ -0,0 +1,33 @@
+﻿window.onload = pageLoad;
+//function () {
+//    alert("now");
+//};
+
+//alert("hello");
+
+
+function pageLoad() {
+    //var lis = document.getElementsByTagName("li");
+    //for (var i = 0; i < lis.length; i++) {
+    //    lis[i].onmouseenter = linkClick;
+    //        //= function () {
+    //    //    alert(this.innerText);
+    //    //};
+    //}
+
+    var ul = document.getElementById("links");
+    ul.onclick = linkClick;
+}
+
+function linkClick(e) {
+    e = e || window.event;
+    var t = e.target || e.srcElement;
+    var c = e.currentTarget;
+    //c.style.backgroundColor = "#F00";
+    if (t.tagName === "A") {
+        e.stopPropogation();
+        e.cancelBubble = true;
+    }
+    alert(t.tagName + " | " + c.tagName );
+}
+
