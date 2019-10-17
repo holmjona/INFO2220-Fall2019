0a0680b454e6dd707368f6018b21bc7d8abdf6fb
Jon Holmes <holmjona@isu.edu>
2019-10-17 12:18:51 -0600

    talked about the basics of jQuery.

---
+++ scripts/usingjquery.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    //var f = $("p");
+    //var p = f.parents("div");
+    //var s = "";
+    //for (var i = 0; i < p.length; i++) {
+    //    s += p[i].id + "\r\n";
+    //}
+    //alert(s);
+   // alert("Hello first");
+    $("li").addClass("selected");
+    $("li").click(liClick);
+    var lis = $("def");
+    lis.hide();
+    var a = 5;
+});
+
+function liClick() {
+    alert(this.tagName);
+}
+//$(document).ready(function () {
+//    alert("Hello second");
+//});
