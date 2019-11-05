88f3e3cda2dfcbfd0849570135fb44831c018d2a
Jon Holmes <holmjona@isu.edu>
2019-11-05 12:16:21 -0700

    Playing with Cookies and Storage.

---
+++ scripts/storage.js
@@ -0,0 +1,19 @@
+﻿window.onload = function () {
+    this.document.getElementById("btnStoreStuff").onclick = function () {
+        //var n = prompt("What is your name");
+        //var d = new Date();
+        //d.setTime(d.getTime() + 2000); 
+        //document.cookie = "name=" + n + ";expires=" + d + ";";
+        var tBoxValue = document.getElementById("txtValue").value;
+        localStorage.setItem("Name", tBoxValue);
+        localStorage["Nombre"] = tBoxValue;
+        localStorage.Imya = tBoxValue;
+
+    };
+    this.document.getElementById("btnShowStuff").onclick = function () {
+        //var cook = document.cookie;
+        //var cookParts = cook.split(";");
+        //var name = cookParts[0].split("=");
+        //alert(name[1]);
+    };
+};
