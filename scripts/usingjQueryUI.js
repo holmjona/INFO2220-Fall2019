432288ecf93587e048a3148f6a5cfdf6005d626b
Jon Holmes <holmjona@isu.edu>
2019-11-14 12:24:22 -0700

    Added jQueryUI stuff and themes.

---
+++ scripts/usingjQueryUI.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $("#information").accordion({collapsible:false,heightStyle:"fill"});
+    //$("#information").hide();
+    //$("#listOfAnimals").accordion();
+    $("#listOfAnimals").sortable({
+        update: function (e,ele) {
+            alert("You just moved "+ ele.item.text());
+        }
+    });
+    //$("#listOfAnimals").hide();
+
+    //$("#field").datepicker();
+    //$("#field").spinner();
+    //$("#field").hide();
+    $("#infoClasses").tabs();
+    //$("#infoClasses").hide();
+    $("#insects").selectmenu();
+    //$("#insects").parent().hide();
+    $("#myMenu").menu();
+
+
+    $("#btnSwanky").click(function () {
+        $("#theme").attr("href", "styles/jquery-ui-swanky.theme.css");
+    });
+    $("#btnSunny").click(function () {
+        $("#theme").attr("href", "styles/jquery-ui-sunny.theme.css");
+    });
+
+});
